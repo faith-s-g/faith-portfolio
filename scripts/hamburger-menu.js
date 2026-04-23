2ef9d9e9efd18625fd3000ab94dec2926f9c977f
faithie27 <faithpiano18@gmail.com>
2026-04-23 14:17:02 +0000

    hamburger page load and resize code

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -3,22 +3,50 @@ $("#hamburger-button").on("click", function () { // ← click event
 
     // ↓ action snippets ↓
     if ($("#nav-menu").hasClass("hidden")) { // ← element has css class
-
         // ↓ action snippets (class on element) ↓
         $("#nav-menu").removeClass("hidden");
-
         // ↑ action snippets (class on element) ↑
-
     } else { // ← element has css class
-
         // ↓ action snippets (class not on element) ↓
         $("#nav-menu").addClass("hidden");
-
-
         // ↑ action snippets (class not on element) ↑
-
     } // ← element has css class
-
     // ↑ action snippets ↑
-
 }); // ← click event
+
+//page loading code
+$("index.html").ready(function () { // ← page ready event
+
+    if (window.matchMedia("(max-width: 700)").matches) { // ← get browser width
+        // ↓ action snippets (< width) ↓
+        $("#nav-menu").addClass("hidden");
+        // ↑ action snippets (< width) ↑
+    } else { // ← get browser width
+        // ↓ action snippets (> width) ↓
+        $("#nav-menu").addClass("hidden");
+        // ↑ action snippets (> width) ↑
+    } // ← get browser width
+}); // ← page ready event
+
+
+//page resizing code
+//on window resize:
+// if window is narrow:
+//  add .hidden to #nav-menu
+// else if window is wide:
+// remove .hidden from #nav-menu
+
+$(window).on("resize", function () { // ← resize browser window event
+
+    // ↓ action snippets ↓
+    if (window.matchMedia("(max-width: 700)").matches) { // ← get browser width
+        // ↓ action snippets (< width) ↓
+        $("#nav-menu").addClass("hidden");
+        // ↑ action snippets (< width) ↑
+    } else { // ← get browser width
+        // ↓ action snippets (> width) ↓
+        $("#nav-menu").removeClass("hidden");
+        // ↑ action snippets (> width) ↑
+    } // ← get browser width
+    // ↑ action snippets ↑
+}); // ← resize browser window event
