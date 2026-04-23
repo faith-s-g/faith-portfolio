d2f5c895a1b104a9c07d3016adef1e3bdd69fac1
faithie27 <faithpiano18@gmail.com>
2026-04-23 00:44:29 +0000

    proj modal 3

--- scripts/modal.js
+++ scripts/modal.js
@@ -32,8 +32,28 @@ $("#proj-button-2").on("click", function () { // ← click event
 
 }); // ← click event
 
-/*close modal code */
+/*clicking project 3 button modal code*/
+$("#proj-button-3").on("click", function () { // ← click event
 
+    // ↓ action snippets ↓
+    $("#modal-overlay").removeClass("hidden");
+    console.log("good");
+    $("#modal-content-3").removeClass("hidden");
+    console.log("good");
+    $("#modal-content-1").addClass("hidden");
+    console.log("good");
+    $("#modal-content-2").addClass("hidden");
+    console.log("good");
+
+
+
+    // ↑ action snippets ↑
+
+}); // ← click event
+
+
+
+/*close button modal code */
 $("#modal-close").on("click", function () { // ← click event
 
     // ↓ action snippets ↓
