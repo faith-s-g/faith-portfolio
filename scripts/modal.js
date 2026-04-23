d1d69a2128e8b6de94b71bdcae4068af54094368
faithie27 <faithpiano18@gmail.com>
2026-04-23 00:41:42 +0000

    proj modal 2

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,11 +4,27 @@ $("#proj-button-1").on("click", function () { // ← click event
     // ↓ action snippets ↓
     $("#modal-overlay").removeClass("hidden");
     console.log("modal overlay loaded");
-    $("#project-modal").removeClass("hidden");
     $("#modal-content-1").removeClass("hidden");
     console.log("modal content 1 shown");
     $("#modal-content-2").addClass("hidden");
     console.log("modal content 2 hidden");
+    $("#modal-content-3").addClass("hidden");
+    console.log("modal content 3 hidden");
+
+    // ↑ action snippets ↑
+
+}); // ← click event
+
+/*clicking project 2 button modal code*/
+$("#proj-button-2").on("click", function () { // ← click event
+
+    // ↓ action snippets ↓
+    $("#modal-overlay").removeClass("hidden");
+    console.log("modal overlay loaded");
+    $("#modal-content-2").removeClass("hidden");
+    console.log("modal content 2 shown");
+    $("#modal-content-1").addClass("hidden");
+    console.log("modal content 1 hidden");
     $("#modal-content-3").addClass("hidden");
     console.log("modal content 3 hidden");
 
