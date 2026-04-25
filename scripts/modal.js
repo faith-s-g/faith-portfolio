8bdba2c969f09a86055d71f3b23eda3b3b939403
faithie27 <faithpiano18@gmail.com>
2026-04-25 18:11:37 +0000

    delete console logs

--- scripts/modal.js
+++ scripts/modal.js
@@ -3,13 +3,9 @@ $("#proj-button-1").on("click", function () { // ← click event
 
     // ↓ action snippets ↓
     $("#modal-overlay").removeClass("hidden");
-    console.log("modal overlay loaded");
     $("#modal-content-1").removeClass("hidden");
-    console.log("modal content 1 shown");
     $("#modal-content-2").addClass("hidden");
-    console.log("modal content 2 hidden");
     $("#modal-content-3").addClass("hidden");
-    console.log("modal content 3 hidden");
 
     // ↑ action snippets ↑
 
@@ -20,13 +16,9 @@ $("#proj-button-2").on("click", function () { // ← click event
 
     // ↓ action snippets ↓
     $("#modal-overlay").removeClass("hidden");
-    console.log("modal overlay loaded");
     $("#modal-content-2").removeClass("hidden");
-    console.log("modal content 2 shown");
     $("#modal-content-1").addClass("hidden");
-    console.log("modal content 1 hidden");
     $("#modal-content-3").addClass("hidden");
-    console.log("modal content 3 hidden");
 
     // ↑ action snippets ↑
 
@@ -37,16 +29,9 @@ $("#proj-button-3").on("click", function () { // ← click event
 
     // ↓ action snippets ↓
     $("#modal-overlay").removeClass("hidden");
-    console.log("good");
     $("#modal-content-3").removeClass("hidden");
-    console.log("good");
     $("#modal-content-1").addClass("hidden");
-    console.log("good");
     $("#modal-content-2").addClass("hidden");
-    console.log("good");
-
-
-
     // ↑ action snippets ↑
 
 }); // ← click event
@@ -62,5 +47,3 @@ $("#modal-close").on("click", function () { // ← click event
     // ↑ action snippets ↑
 
 }); // ← click event
-
-
